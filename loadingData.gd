38f71b7de3ad9a77c01d4c18e9634d9bfaca183d
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-04-19 18:08:06 +0200

    Delete loadingData.gd

--- loadingData.gd
+++
@@ -1,6 +0,0 @@
-class_name LoadingData extends Resource
-
-@export var main_scene_path: String = "res://main/levels/mainlevel.tscn"
-@export var prefabs_to_load: Array[PackedScene] = []
-@export var meshes_to_store: Dictionary = {}
-@export var materials_to_compile: Array[Material] = []
